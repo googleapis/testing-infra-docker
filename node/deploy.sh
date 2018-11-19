86cf5b33389b674eb38e316b27ed1eab79694a86
Jonathan Lui <jonathanlui@google.com>
2018-11-19 12:39:54 -0800

    node: update deploy.sh
    Change-Id: I52440a5e9bf29e87a67794f2797fed9e38aac785

--- node/deploy.sh
+++ node/deploy.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-docker build -t gcr.io/cloud-devrel-kokoro-resources/nodejs-test .
-docker push gcr.io/cloud-devrel-kokoro-resources/nodejs-test
+docker build -t gcr.io/cloud-devrel-kokoro-resources/node:6-user 6-user
+docker build -t gcr.io/cloud-devrel-kokoro-resources/node:8-user 8-user
+docker build -t gcr.io/cloud-devrel-kokoro-resources/node:10-user 10-user
+docker build -t gcr.io/cloud-devrel-kokoro-resources/node:11-user 11-user
+
+docker push gcr.io/cloud-devrel-kokoro-resources/node
