63bd0fad4845fcd4bea243642164ac367634df37
Bu Sun Kim <busunkim@google.com>
2018-12-21 14:19:38 -0800

    Fix typo in build.sh

--- .kokoro/build.sh
+++ .kokoro/build.sh
@@ -14,8 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -eo pipefail
-
 # Disable buffering, so that the logs stream through.
 export PYTHONUNBUFFERED=1
 
